efd80af59d9ddf86f1a54c49ecb2c990d0d719bf
Potapov-VI <Potapov-VI@Potapov-VI.permenergo.local>
2019-02-06 17:09:36 +0500

    Пример выделение, изменение размеров 2

--- LineDrawer/src/linedrawer/LineDrawer.java
+++ LineDrawer/src/linedrawer/LineDrawer.java
@@ -11,15 +11,69 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class LineDrawer extends Application {
-
+//    
+//    class LineB{
+//            
+//            void create(int x1, int y1, int x2, int y2){
+//                int A = y2 - y1;
+//                int B = x1 - x2;
+//                int Sign;
+//                int SignA;
+//                int SignB;
+//                int f;
+//                int X;
+//                int Y;
+//                
+//                if  (Math.abs(A) >  Math.abs(B)) {
+//                    Sign = 1;
+//                } else {
+//                    Sign = -1;
+//                }
+//                if (A < 0) {SignA = -1;} else {SignA = 1;} 
+//                if (B < 0) {SignB = -1;} else {SignB = 1;} 
+//                f = 0;
+//                X = x1;
+//                Y = y1;
+//                gc.fillRect(X,  Y, 1, 1);
+//                if  (Sign == -1) {
+//                    do {
+//                    f = f + A*SignA;
+//                        if (f > 0) {
+//                            f = f - B*SignB;
+//                            Y = Y + SignA;
+//                        } 
+//                    X = X - SignB;
+//                    gc.fillRect(X,  Y, 1, 1);
+//                    
+//
+//                    } while (X != x2 || Y != y2);
+//                } else
+//                    do {
+//                    f = f + B*SignB;
+//                        if (f > 0) {
+//                            f = f - A*SignA;
+//                            X = X - SignB;
+//                        } 
+//                    Y = Y + SignA;
+//                    gc.fillRect(X,  Y, 1, 1);    
+//                    } while (X != x2 || Y != y2);
+//                }
+//        
+//        }       
+    
+    
+    
+    
     public static void main(String[] args) {
         launch(args);
     }
 
     private Line curLine;
+//    private LineB lineB;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -32,7 +86,10 @@ public class LineDrawer extends Application {
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
         scrollPane.setStyle("-fx-focus-color: transparent;");
-
+        
+        
+        
+        
         drawingPane.setOnMousePressed(event -> {
             if (!event.isPrimaryButtonDown()) {
                 return;
